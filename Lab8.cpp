574550f8989db6aab13cdba3dacbb484b330ee9b
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2025-07-15 12:44:32 -0400

    Initial Commit

--- Lab8.cpp
+++ Lab8.cpp
@@ -1,12 +1,8 @@
-// The factorial of a number n is the product of all numbers from 1 up to the number n.
-// For example n=5 then the factorial of 5 will be 1*2*3*4*5= 120, so 5!= 120.
-// Write a program that prompts a user to enter a positive integer less than 13 
-// then uses a for loop to calculate the factorial of n and print it to the screen.
-
 #include <iostream>
 using namespace std;
 
 int main() {
+    // Place your code for the Factorial Calculator program here
 
     return 0;
 }
